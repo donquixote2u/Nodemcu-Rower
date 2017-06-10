d5bebefda8ded689fecd5ae8c5a8c4b5262c2a91
donquixote2u <donquixote2u@gmail.com>
2017-06-10 13:00:24 +1200

    modified:   Rower.lua 	new file:   samplemenu.lua 	new file:   screen.lua

--- Rower.lua
+++ Rower.lua
@@ -1,12 +1,11 @@
 --[[  Rower Mk 3 - by Bruce Woolmore 7/4/2017
-  This version for Wemos D1 Mini (ESP8266 dev bd) running Nodemcu lua env 
-  vsn 1.5.4.1 custom compile with math,ucglib 
+This version for Wemos D1 Mini (ESP8266 dev bd) running Nodemcu vsn 1.5.4.1 custom compile with math,ucglib 
  counts all Hi to Lo state changes in the Hall effect sensor
-ILI9341 screen driven by SPI ; board pin map as follows:
-D0/lua0->screen RST,D5/lua5->SCK,D6/lua6->MISO,D7/lua7->MOSI
-D8/lua8->CS, D4/lua4->D/C,3v3->VCC,LED(via pot), bd GND-> screen GND,
+ ILI9341 screen driven by SPI ; board pin map as follows:
+   D0/lua0->screen RST,D5/lua5->SCK,D6/lua6->MISO,D7/lua7->MOSI
+   D8/lua8->CS, D4/lua4->D/C,3v3->VCC,LED(via pot), bd GND-> screen GND,
 other connections:
-pgm switches (to be added) D3/lua3->PGM1 D2/lua2->PGM2
+pgm switches (to be added) D4/gpio3->PGM1 D3/gpio2->PGM2
 D1/lua1->Hall effect sensor pin  (pulled up via 4k7 resistor to 3v3
 --]]
 function CalcSpeed()
@@ -14,7 +13,11 @@ function CalcSpeed()
  -- on interrupt from Hall Effect Sensor pin, calc elapsed time since last int
 msNow=tmr.now()
 if (lastPulse ~= 0) then -- ignore first pulse in stroke
-   pulseElapsed=msNow-lastPulse -- calc period between pulses
+   local pulseElapsed=msNow-lastPulse -- calc period between pulses
+   pulseCount=pulseCount+1
+   strokeElapsed=strokeElapsed+pulseElapsed
+   totDistance=totDistance+(pulseDistance/100) -- distance in metres
+   totTime=totTime+(pulseElapsed / 1000.0)   -- time in seconds
 end   
 lastPulse=msNow -- save reading as Last
 end 
@@ -29,48 +32,29 @@ end
 function disInt()
      gpio.mode(SENSEPIN, gpio.INPUT)
 end
-  
--- start here ; intit constants, variables, set up sensor pin interrupts
-sessionTimeout=5000     --// timeout in ms to detect end of session
-strokeTimeout=1500   --// timeout in ms to detect end of stroke
-pulseDistance=20.0  --// distance travelled in cm between each pulse
-dofile("screen.lua")
-init_display() -- set up display screen ready to show data
---init state variables:
-pulseElapsed=0
-lastPulse = 0      -- previous sensor timestamp 
-strokeElapsed = 0  -- ms since end of last stroke
-strokeCount=0 
-pulseCount=0
-totDistance=0
-totTime=0
-startTime=0
-SENSEPIN = 1
-enInt()         -- turn sensor interrupt on D1 (gpio4) on
-tmr.alarm( 1, 500, 1,  function() waitloop() end)
 
-function waitloop() -- runs every 500ms
+function resetCounts() 
+--init state variables:
+  pulseElapsed=0
+  lastPulse = 0      -- previous sensor timestamp 
+  strokeElapsed = 0  -- ms since end of last stroke
+  strokeCount=0 
+  pulseCount=0
+  totDistance=0
+  totTime=0
+  startTime=0
+end  
+
+function waitloop() -- runs every 500ms from main timer
  msNow=tmr.now()
  if(startTime==0) then  startTime=msNow end  -- capture start
  if(lastPulse~=0) then timeElapsed=msNow-lastPulse
  else timeElapsed=0 end 
- if(pulseElapsed>0) then
-   updateData() -- process last pulse period 
- end  
  if(timeElapsed>sessionTimeout) then sessionEnd() 
  else   -- in session
    if(timeElapsed>strokeTimeout) then  strokeEnd() end 
  end -- timeouts
 end -- function  
-
- function updateData()
-   print("ms="..pulseElapsed.."\r\n")
-   pulseCount=pulseCount+1
-   strokeElapsed=strokeElapsed+pulseElapsed
-   totDistance=totDistance+(pulseDistance/100) -- distance in metres
-   totTime=totTime+(pulseElapsed / 1000.0)   -- time in seconds
-   pulseElapsed=0
- end
 
  function strokeEnd() 
    disInt()     -- disable interrupt
@@ -110,3 +94,14 @@ function sessionEnd()
  totDistance=0.0
  totTime=0.0
  end
+
+-- start here ; intit constants, variables, set up sensor pin interrupts
+sessionTimeout=5000     --// timeout in ms to detect end of session
+strokeTimeout=1500   --// timeout in ms to detect end of stroke
+pulseDistance=20.0  --// distance travelled in cm between each pulse
+SENSEPIN = 1
+dofile("screen.lua")
+init_display() -- set up display screen ready to show data
+enInt()         -- turn sensor interrupt on D1 (gpio4) on
+resetCounts()
+tmr.alarm( 1, 500, 1,  function() waitloop() end)
---
+++ screen.lua
@@ -0,0 +1,46 @@
+-- UCGLIB SCREEN HANDLING FOR ILI9341
+function init_display()
+   -- Hardware SPI CLK=GPIO14 MOSI=GPIO13 MISO=GPIO12 (not used)
+   -- CS, D/C, and RES can be assigned freely to available GPIOs
+    local cs  = 8 -- GPIO15, pull-down 10k to GND
+    local dc  = 4 -- GPIO2
+    local res = 0 -- GPIO16
+    spi.setup(1, spi.MASTER, spi.CPOL_LOW, spi.CPHA_LOW, 8, 8)
+    -- initialize the matching driver for your display
+    disp = ucg.ili9341_18x240x320_hw_spi(cs, dc, res)
+     -- disp:begin(ucg.FONT_MODE_TRANSPARENT)
+    disp:begin(ucg.FONT_MODE_SOLID)
+    disp:setRotate270()
+    disp:clearScreen()
+    -- set up default colours white font black bg
+    disp:setColor(0, 255, 255, 255)
+    disp:setColor(1, 0, 0, 0)
+    disp:setPrintDir(0)
+end
+-- ------ SCREEN DISPLAY HANDLER -----------
+function dprint(font_index,content)
+  if(content) then  
+    if      (font_index==2) then
+    disp:setFont(ucg.font_helvB24_hr)
+    else -- default 
+    font_index=1  
+    disp:setFont(ucg.font_helvB18_hr)
+    -- disp:setFont(ucg.font_helvB12_hr)
+    end
+   --local slen=disp:drawString(Scrxpos, Scrypos,0,content)
+   -- increment screen x position
+   --Scrxpos=Scrxpos+slen
+   disp:setPrintPos(Scrxpos,Scrypos)
+   disp:print(content)
+   Scrxpos=Scrxpos+(string.len(content)*(font_index+1)*6)
+   if(Scrxpos>320) then Scrxpos=10 end
+  end 
+end
+--    ==================================================
+function dprintl(font_index,content)
+dprint(font_index,content)
+local spacer=(6*(font_index+2))+10 -- calc y spacing to next line
+Scrypos=Scrypos+spacer
+Scrxpos=10
+if(Scrypos>240) then Scrypos=10 end
+end
